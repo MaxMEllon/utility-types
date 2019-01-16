91d19791c1b3250a8bfc21494f70da08b3801b0b
MaxMEllon <maxmellon1994@gmail.com>
2019-01-17 01:07:58 +0900

    :tada: first commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+/**
+ * Get return type of async function
+ *
+ * @typeparam F - typeof async function
+ * @example
+ *
+ * const plusOne = async (a: number) => a + 1
+ * type Response = AsyncFunctionReturnType<typeof plusOne>
+ *      // => number
+ */
+export type AsyncFunctionReturnType<F> = F extends () => Promise<infer R> ? R : never
+
+/**
+ * Get resolve type of promise
+ *
+ * @typeparam P - typeof Promise
+ * @example
+ *
+ * const promise = axios<Response>(...)
+ *
+ * type Resolve = PromiseResolveType<typeof promise>
+ *      // => Response
+ */
+export type PromiseResolveType<P> = P extends Promise<infer R> ? R : never
+
+/**
+ * Set nullable property
+ *
+ * @example
+ * type Tree<T> = {
+ *    value: T
+ * } & Nullable<{
+ *    left: Tree<T>
+ *    right: Tree<T>
+ * }>
+ */
+export type Nullable<T> = { [P in keyof T]: T[P] | null }
+
+/**
+ * @example
+ * type Tree<T> = {
+ *    value: T
+ * } & Partial<{
+ *    left: Tree<T>
+ *    right: Tree<T>
+ * }
+ */
+export type Partial<T> = { [P in keyof T]?: T[P] }
